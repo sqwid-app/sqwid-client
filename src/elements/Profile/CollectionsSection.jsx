f60e0cc077adb54ffebbc9f141f605279dac43da
boidushya <boidushyabhattacharya@gmail.com>
2021-10-22 11:31:04 +0530

    fix: changed collection thumbnail image

--- src/elements/Profile/CollectionsSection.jsx
+++ src/elements/Profile/CollectionsSection.jsx
@@ -25,15 +25,15 @@ const Title = styled.h1`
 
 const CollectionsSection = () => {
 	const cards = [{
-		src:"https://images.unsplash.com/photo-1634498948540-3d4a2ec4d007?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1072&q=80",
+		src:"https://images.unsplash.com/photo-1634337256330-aae44dc7b8f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1964&q=80",
 		title:"You've got",
 		link:"/"
 	},{
-		src:"https://images.unsplash.com/photo-1634425101299-65e872cdbca6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+		src:"https://images.unsplash.com/photo-1634840150834-f6d4544f5b9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1927&q=80",
 		title:"A friend",
 		link:"/"
 	},{
-		src:"https://images.unsplash.com/photo-1634370058500-ebaeece3e395?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=327&q=80",
+		src:"https://images.unsplash.com/photo-1634819875292-83e02980a99b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1887&q=80",
 		title:"In me",
 		link:"/"
 	}]
