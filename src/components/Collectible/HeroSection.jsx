601f15bc9516df7f4ec5bfc6db740b26fd90c021
boidushya <boidushyabhattacharya@gmail.com>
2021-11-01 02:16:37 +0530

    feat: added plyr

--- src/components/Collectible/HeroSection.jsx
+++ src/components/Collectible/HeroSection.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { useParams } from "react-router";
 import styled from "styled-components";
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+	padding: 0 6rem;
+	height: 75vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
 
 const HeroSection = () => {
 	const { addr } = useParams()
 	return (
 		<Wrapper>
-			ok {addr}
+			pog
 		</Wrapper>
 	)
 }
