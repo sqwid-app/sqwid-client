7a15322d04995f3987f016c44ab2f0123f394e84
boidushya <boidushyabhattacharya@gmail.com>
2021-11-01 17:22:18 +0530

    fix: removed unwanted comments

--- src/elements/Profile/CollectionsSection.jsx
+++ src/elements/Profile/CollectionsSection.jsx
@@ -64,11 +64,6 @@ const CollectionsSection = () => {
 		})
 	//eslint-disable-next-line
 	}, [])
-	// {
-	// 	src:"https://images.unsplash.com/photo-1634819875292-83e02980a99b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1887&q=80",
-	// 	title:"In me",
-	// 	link:"/"
-	// }
 	return (
 		<Wrapper>
 			<CustomScrollbar autoHide>
