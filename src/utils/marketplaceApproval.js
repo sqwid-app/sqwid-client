2faa8dda4c49b1751a6f865f1b15b01dc5c30639
andithemudkip <andithemudkip@gmail.com>
2021-11-03 00:14:10 +0200

    feat: added marketplace approval utils

---
+++ src/utils/marketplaceApproval.js
@@ -0,0 +1,32 @@
+import { ethers } from 'ethers';
+import { Interact } from './connect';
+import contractABI from '../constants/contracts/SqwidERC1155';
+
+const approveMarketplace = async () => {
+    let { signer } = await Interact ();
+
+    let contract = new ethers.Contract (
+        process.env.REACT_APP_COLLECTIBLE_CONTRACT_ADDRESS,
+        contractABI,
+        signer
+    );
+
+    const tx = await contract.setApprovalForAll (process.env.REACT_APP_MARKETPLACE_CONTRACT_ADDRESS, true);
+    return await tx.wait ();
+};
+
+const isMarketplaceApproved = async () => {
+    let { signer } = await Interact ();
+    const address = await signer.getAddress ();
+
+    let contract = new ethers.Contract (
+        process.env.REACT_APP_COLLECTIBLE_CONTRACT_ADDRESS,
+        contractABI,
+        signer
+    );
+
+    const isApproved = await contract.isApprovedForAll (address, process.env.REACT_APP_MARKETPLACE_CONTRACT_ADDRESS);
+    return isApproved;
+};
+
+export { approveMarketplace, isMarketplaceApproved };
