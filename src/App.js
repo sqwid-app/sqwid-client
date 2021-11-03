76dc91948c4419dc9def20966ebaccf9015bee7d
boidushya <boidushyabhattacharya@gmail.com>
2021-11-03 12:07:45 +0530

    fix: multiple routes rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Suspense } from "react";
 import AuthProvider from "@contexts/Auth/AuthProvider";
-import {
-	BrowserRouter as Router,
-	Switch,
-} from "react-router-dom";
 import FullPageLoading from "@elements/Default/FullPageLoading";
 const Routes = React.lazy(() => import("./routes"))
 
 const App = () => {
 	return (
 		<AuthProvider>
-			<Router>
-				<Switch>
-					<Suspense fallback={<FullPageLoading init/>}>
-						<Routes />
-					</Suspense>
-				</Switch>
-			</Router>
+			<Suspense fallback={<FullPageLoading init/>}>
+				<Routes />
+			</Suspense>
 		</AuthProvider>
 	)
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,41 +4,46 @@ import Create from "@pages/Create";
 import Landing from "@pages/Landing";
 import Profile from "@pages/Profile";
 import NotFound from "@pages/NotFound";
-import { Route } from "react-router-dom"
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom"
 
 const Routes = () => {
 	return (
-		<>
-			<Route
-				path="/"
-				exact
-				component={Landing}
-			/>
-			<Route
-				path="/collections/:id?"
-				exact
-				component={Collections}
-			/>
-			<Route
-				path="/profile/:id?"
-				exact
-				component={Profile}
-			/>
-			<Route
-				path="/create"
-				exact
-				component={Create}
-			/>
-			<Route
-				path="/collectible/:addr/:ownerID?"
-				exact
-				component={Collectible}
-			/>
-			<Route
-				path="*"
-				component={NotFound}
-			/>
-		</>
+		<Router>
+			<Switch>
+				<Route
+					path="/"
+					exact
+					component={Landing}
+				/>
+				<Route
+					path="/collections/:id?"
+					exact
+					component={Collections}
+				/>
+				<Route
+					path="/profile/:id?"
+					exact
+					component={Profile}
+				/>
+				<Route
+					path="/create"
+					exact
+					component={Create}
+				/>
+				<Route
+					path="/collectible/:addr/:ownerID?"
+					exact
+					component={Collectible}
+				/>
+				<Route
+					component={NotFound}
+				/>
+			</Switch>
+		</Router>
 	)
 }
 
