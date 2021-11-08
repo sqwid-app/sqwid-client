5b5387ae05661953409e19655e0f48fdf08184f8
andithemudkip <andithemudkip@gmail.com>
2021-11-08 15:46:02 +0200

    fix collected nfts

--- src/elements/Profile/OwnedNFTSection.jsx
+++ src/elements/Profile/OwnedNFTSection.jsx
@@ -3,6 +3,7 @@ import CardSectionContainer from "@elements/Default/CardSectionContainer";
 import { respondTo } from "@styles/styledMediaQuery";
 import AuthContext from "@contexts/Auth/AuthContext";
 import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router";
 import { Suspense } from "react";
 import styled from "styled-components";
 import axios from 'axios';
@@ -30,21 +31,23 @@ const Header = styled.h1`
 
 const OwnedNFTSection = () => {
 	const { auth } = useContext (AuthContext);
+	const { id } = useParams()
 	const [items, setItems] = useState([]);
 
 	useEffect (() => {
+		let addressToCheck = id || auth?.evmAddress;
 		const fetchData = async () => {
-			const result = await axios (`${process.env.REACT_APP_API_URL}/get/r/marketplace/fetchMarketItems/owner/${auth.evmAddress}`);
+			const result = await axios (`${process.env.REACT_APP_API_URL}/get/r/marketplace/fetchMarketItems/owner/${addressToCheck}`);
 			let items = result.data;
 			setItems (items);
 		}
 		fetchData ();
-	}, [auth.evmAddress]);
+	}, [auth, auth?.evmAddress, id]);
 	return (
 		<>
 			{items.length!==0&&(
 				<Wrapper>
-					<Header>Owned NFTs </Header>
+					<Header>Collected</Header>
 					<CardSectionContainer>
 						<Suspense>
 							{items.map((item,index)=>(
