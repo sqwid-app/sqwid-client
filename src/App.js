439705eab5fa1104828607bd422d638371130b3b
boidushya <boidushyabhattacharya@gmail.com>
2022-02-10 04:16:36 +0530

    fix: mfw it didnt work and we are back to static meta

--- src/App.js
+++ src/App.js
@@ -4,33 +4,9 @@ import FullPageLoading from "@elements/Default/FullPageLoading";
 import Helmet from "react-helmet";
 const Routes = React.lazy(() => import("./routes"))
 
-const MetaTags = () => {
-	return (
-		<Helmet>
-			<title>Sqwid – An ocean of possibilities</title>
-			<meta name="title" content="Sqwid – An ocean of possibilities" />
-			<meta name="description"
-				content="Sqwid is an NFT marketplace running on the Reef chain that features per-item customizable royalties." />
-			<meta property="og:type" content="website" />
-			<meta property="og:url" content="https://sqwid.app/" />
-			<meta property="og:title" content="Sqwid – An ocean of possibilities" />
-			<meta property="og:description" content="Sqwid is an NFT marketplace running on the Reef chain that features per-item customizable royalties." />
-			<meta property="og:site_name" content="Sqwid" />
-			<meta property="og:image" content="https://sqwid.app/landing.png" />
-			<meta property="twitter:card" content="summary_large_image" />
-			<meta property="twitter:url" content="https://sqwid.app/" />
-			<meta property="twitter:title" content="Sqwid – An ocean of possibilities" />
-			<meta property="twitter:description" content="Sqwid is an NFT marketplace running on the Reef chain that features per-item customizable royalties." />
-			<meta property="twitter:image" content="https://sqwid.app/landing.png" />
-		</Helmet>
-	)
-}
-
-
 const App = () => {
 	return (
 		<AuthProvider>
-			<MetaTags />
 			<Suspense fallback={<FullPageLoading init component="app" />}>
 				<Routes />
 			</Suspense>
