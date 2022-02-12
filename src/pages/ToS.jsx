23425736d206b25d2b52dd9d66b2af4a65cb33de
boidushya <boidushyabhattacharya@gmail.com>
2022-02-12 23:50:08 +0530

    fix: slight modifications to ToS content

--- src/pages/ToS.jsx
+++ src/pages/ToS.jsx
@@ -45,33 +45,33 @@ const ToS = () => {
 
 				<h2>2. Use License</h2>
 
-				<p>Permission is granted to temporarily download one copy of the materials on sqwid.app's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+				<p>Permission is granted to temporarily download one copy of the materials on sqwid's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
 
 				<ul>
 					<li>modify or copy the materials;</li>
 					<li>use the materials for any commercial purpose or for any public display;</li>
-					<li>attempt to reverse engineer any software contained on sqwid.app's Website;</li>
+					<li>attempt to reverse engineer any software contained on sqwid's Website;</li>
 					<li>remove any copyright or other proprietary notations from the materials; or</li>
 					<li>transferring the materials to another person or "mirror" the materials on any other server.</li>
 				</ul>
 
-				<p>This will let sqwid.app to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.</p>
+				<p>This will let sqwid to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.</p>
 
 				<h2>3. Disclaimer</h2>
 
-				<p>All the materials on sqwid.app’s Website are provided "as is". sqwid.app makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, sqwid.app does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
+				<p>All the materials on sqwid’s Website are provided "as is". Sqwid makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, sqwid does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
 
 				<h2>4. Limitations</h2>
 
-				<p>sqwid.app or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on sqwid.app’s Website, even if sqwid.app or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
+				<p>sqwid.app or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on sqwid’s Website, even if sqwid or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
 
 				<h2>5. Revisions and Errata</h2>
 
-				<p>The materials appearing on sqwid.app’s Website may include technical, typographical, or photographic errors. sqwid.app will not promise that any of the materials in this Website are accurate, complete, or current. sqwid.app may change the materials contained on its Website at any time without notice. sqwid.app does not make any commitment to update the materials.</p>
+				<p>The materials appearing on sqwid’s Website may include technical, typographical, or photographic errors. Sqwid will not promise that any of the materials in this Website are accurate, complete, or current. Sqwid may change the materials contained on its Website at any time without notice. Sqwid does not make any commitment to update the materials.</p>
 
 				<h2>6. Links</h2>
 
-				<p>sqwid.app has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by sqwid.app of the site. The use of any linked website is at the user’s own risk.</p>
+				<p>sqwid.app has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by sqwid of the site. The use of any linked website is at the user’s own risk.</p>
 
 				<h2>7. Site Terms of Use Modifications</h2>
 
@@ -83,7 +83,7 @@ const ToS = () => {
 
 				<h2>9. Governing Law</h2>
 
-				<p>Any claim related to sqwid.app's Website shall be governed by the laws of ve without regards to its conflict of law provisions.</p>
+				<p>Any claim related to sqwid's Website shall be governed by the laws of ve without regards to its conflict of law provisions.</p>
 			</Container>
 		</Wrapper>
 	)
