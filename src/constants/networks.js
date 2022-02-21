40b411220504d67e3a6ed952f7f89ca49fc34302
andithemudkip <andithemudkip@gmail.com>
2022-02-22 01:18:53 +0200

    fix: revert testnet contracts

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -2,9 +2,9 @@ export const networks = {
 	reef_testnet: {
 		rpc: 'wss://rpc-testnet.reefscan.com/ws',
 		contracts: {
-			marketplace: '0xEde8b3844E01444a9D3E0aCDd118f97827f179A4',
-			erc1155: '0x03aE38D60a5F97a747980d6EC4B1CdDAAb9F1979',
-			utility: '0x7E2f35C171Ea6B96B45acBC079D391e06097a5E0'
+			marketplace: '0xbfe17d89845F2dCA11EE8C26e98ea59a67631Df3',
+			erc1155: '0xA9EF55E0987E12F82D05A01c72b683Af43c70938',
+			utility: '0x7AB030fA1953074762484167D0BA48C1bEd20CF7'
 		},
 		backend: 'https://testnet-api.sqwid.app'
 	},
