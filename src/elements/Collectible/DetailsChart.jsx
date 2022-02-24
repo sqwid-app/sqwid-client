5be5f24b66b909b60d9dd3d90bd3652ada5d4240
boidushya <boidushyabhattacharya@gmail.com>
2022-02-25 02:01:13 +0530

    fix: added shadows to history chart :)

--- src/elements/Collectible/DetailsChart.jsx
+++ src/elements/Collectible/DetailsChart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import {
 	Chart as ChartJS,
 	CategoryScale,
@@ -8,20 +8,21 @@ import {
 	Tooltip,
 	Legend,
 	Filler,
-	defaults
-} from "chart.js";
-import { Chart } from "react-chartjs-2";
-import ZoomPlugin from "chartjs-plugin-zoom";
+	defaults,
+	LineController,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+import ZoomPlugin from 'chartjs-plugin-zoom';
 
 const Utils = {
-	id: "utils",
+	id: 'utils',
 	beforeInit: function (chart) {
 		const originalFit = chart.legend.fit;
 		chart.legend.fit = function fit() {
 			originalFit.bind(chart.legend)();
 			this.height += 10;
 		};
-	}
+	},
 };
 
 ChartJS.register(
@@ -40,13 +41,13 @@ const DetailsChart = () => {
 	const chartRef = useRef();
 
 	const labels = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July"
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
 	];
 
 	const randomNumberInRange = (min, max) =>
@@ -57,10 +58,10 @@ const DetailsChart = () => {
 			.fill(0)
 			.map(() => randomNumberInRange(options.min, options.max));
 
-	const accentColor = "13, 104, 216";
-	const borderColor = "120, 121, 135";
+	const accentColor = '13, 104, 216';
+	const borderColor = '120, 121, 135';
 
-	defaults.animation.easing = "easeOutQuart";
+	defaults.animation.easing = 'easeOutQuart';
 	defaults.animation.duration = 750;
 	defaults.font.family = `"Nunito Sans", "Segoe UI", Tahoma, Geneva, Verdana, sans-serif`;
 	defaults.font.size = 16;
@@ -69,112 +70,96 @@ const DetailsChart = () => {
 	defaults.scale.grid.borderWidth = 4;
 	defaults.scale.grid.borderDash = [6];
 	defaults.scale.grid.color = `rgba(${borderColor},0.125)`;
-	defaults.elements.line.fill = "start";
+	defaults.elements.line.fill = 'start';
 	defaults.elements.line.tension = 0.5;
 	defaults.datasets.line.borderColor = `rgba(${accentColor}, 1)`;
 	defaults.datasets.line.pointBackgroundColor = `rgba(${accentColor}, 1)`;
 	defaults.datasets.line.hoverPointBackgroundColor = `rgba(${accentColor}, 1)`;
-	defaults.datasets.line.pointBorderColor = "rgba(255,255,255,1)";
-
-	// defaults = {
-	// 	...defaults,
-	// 	animation: {
-	// 		easing: "easeOutBounce"
-	// 	},
-	// 	font: {
-	// 		family: `"Nunito Sans", "Segoe UI", Tahoma, Geneva, Verdana, sans-serif`,
-	// 		size: 16,
-	// 		weight: "800"
-	// 	},
-	// 	scale: {
-	// 		grid: {
-	// 			borderColor: `rgb(${borderColor})`,
-	// 			borderWidth: 4,
-	// 			borderDash: [6],
-	// 			color: `rgba(${borderColor},0.125)`
-	// 		}
-	// 	},
-	// 	elements: {
-	// 		line: {
-	// 			fill: "start",
-	// 			tension: 0.5
-	// 		},
-	// 		point: {
-	// 			radius: 6,
-	// 			hoverRadius: 6,
-	// 			borderWidth: 3,
-	// 			hoverBorderWidth: 3
-	// 		}
-	// 	},
-	// 	datasets: {
-	// 		line: {
-	// 			borderColor: `rgba(${accentColor}, 1)`,
-	// 			pointBackgroundColor: `rgba(${accentColor}, 1)`,
-	// 			hoverPointBackgroundColor: `rgba(${accentColor}, 1)`,
-	// 			pointBorderColor: "rgba(255,255,255,1)"
-	// 		}
-	// 	}
-	// }
+	defaults.datasets.line.pointBorderColor = 'rgba(255,255,255,1)';
 
 	const options = {
 		responsive: true,
-		maintainAspectRatio: false,
 		scales: {
 			x: {
 				ticks: {
 					color: `rgba(${borderColor},1)`,
-				}
+				},
 			},
 			y: {
 				ticks: {
 					color: `rgba(${borderColor},1)`,
-				}
+				},
 			},
 		},
 		plugins: {
 			tooltip: {
 				padding: {
 					x: 12,
-					y: 16
+					y: 16,
 				},
-				backgroundColor: "hsla(240, 10%, 16%,0.9)",
+				backgroundColor: 'hsla(240, 10%, 16%,0.9)',
 				borderColor: `rgb(${borderColor})`,
 				borderWidth: 2,
 				boxPadding: 8,
-				bodyColor: "hsl(240, 6%, 75%)",
+				bodyColor: 'hsl(240, 6%, 75%)',
 			},
 			legend: {
-				position: "top",
-				align: "end",
+				position: 'top',
+				align: 'end',
 				labels: {
-					pointStyle: "circle",
+					pointStyle: 'circle',
 					usePointStyle: true,
-					boxWidth: 8
-				}
+					boxWidth: 8,
+				},
 			},
 			zoom: {
 				zoom: {
 					wheel: {
-						enabled: true
+						enabled: true,
 					},
 					pinch: {
-						enabled: true
+						enabled: true,
 					},
 					drag: {
-						enabled: true
+						enabled: true,
 					},
-					mode: "x"
-				}
-			}
-		}
+					mode: 'x',
+				},
+			},
+		},
 	};
 
 	const [data, setData] = useState({
 		labels,
-		datasets: []
+		datasets: [],
 	});
 
 	useEffect(() => {
+		class ShadowLine extends LineController {
+			draw() {
+				super.draw(arguments);
+				const { ctx } = this.chart;
+
+				const originalStroke = ctx.stroke;
+				ctx.stroke = function () {
+					ctx.save();
+					ctx.shadowColor = 'rgba(0,0,0,1)';
+					ctx.shadowBlur = 18;
+					ctx.shadowOffsetX = 0;
+					ctx.shadowOffsetY = 6;
+					originalStroke.apply(this, arguments);
+					ctx.restore();
+				};
+				LineController.prototype.draw.apply(this, arguments);
+				ctx.stroke = originalStroke;
+			}
+		}
+
+		ShadowLine.id = 'ShadowLine';
+		ShadowLine.defaults = LineController.defaults;
+
+		ChartJS.register(ShadowLine);
+
 		const chart = chartRef.current;
 		const gradientBottomOffset = 50;
 
@@ -186,7 +171,7 @@ const DetailsChart = () => {
 			0,
 			chart.height - gradientBottomOffset
 		);
-		gradient.addColorStop(0, `rgba(${accentColor}, 0.5)`);
+		gradient.addColorStop(0, `rgba(${accentColor}, 0.75)`);
 		gradient.addColorStop(0.2, `rgba(${accentColor}, 0.25)`);
 		gradient.addColorStop(1, `rgba(${accentColor}, 0)`);
 
@@ -195,24 +180,21 @@ const DetailsChart = () => {
 			datasets: [
 				{
 					backgroundColor: gradient,
-					label: "Price",
+					label: 'Price',
 					data: getRandomData({
 						size: labels.length,
 						min: 0,
-						max: 1000
-					})
-				}
-			]
+						max: 1000,
+					}),
+				},
+			],
 		});
 		//eslint-disable-next-line
 	}, []);
 
-	// useEffect(() => {
-	// 	console.log(data)
-	// 	//eslint-disable-next-line
-	// }, [])
-
-	return <Chart ref={chartRef} type="line" options={options} data={data} />;
+	return (
+		<Chart ref={chartRef} type="ShadowLine" options={options} data={data} />
+	);
 };
 
-export default DetailsChart
+export default DetailsChart;
