8312e3a1ba505b5309ba259fe7433f5c6f65677e
andithemudkip <andithemudkip@gmail.com>
2022-03-21 15:04:00 +0200

    feat: update testnet contracts

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -2,9 +2,9 @@ export const networks = {
 	reef_testnet: {
 		rpc: "wss://rpc-testnet.reefscan.com/ws",
 		contracts: {
-			marketplace: "0xB2871bF369ce67cc0E251b449fc21A6DbAe93c2e",
+			marketplace: "0xd3202Ee6077C7cc25eAea3aE11bec2cD731D19FC",
 			erc1155: "0x49aC7Dc3ddCAb2e08dCb8ED1F18a0E0369515E47",
-			utility: "0x74d9c321Ec73F717c53788d70357fB146B25f5C4",
+			utility: "0x08925246669D150d5D4597D756A3C788eae2834B",
 		},
 		backend: "https://testnet-api.sqwid.app",
 		// backend: "http://localhost:8080",
