c05fcb94e0338fa488bd4a3c113fc9241244f1eb
andithemudkip <andithemudkip@gmail.com>
2022-06-27 18:37:20 +0300

    fix: update contracts and fix coingecko price

--- src/utils/convertREEFtoUSD.js
+++ src/utils/convertREEFtoUSD.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const convertREEFtoUSD = async price => {
 	try {
 		let res = await axios(
-			"https://api.coingecko.com/api/v3/simple/price?ids=reef-finance&vs_currencies=usd"
+			"https://api.coingecko.com/api/v3/simple/price?ids=reef&vs_currencies=usd"
 		);
 		let conversionRate = res.data["reef-finance"].usd;
 		return Number(price) * Number(conversionRate);
