72bfe502fbf1ce3eb739d7154b82d90feaa181c6
andithemudkip <andithemudkip@gmail.com>
2022-09-05 19:11:43 +0300

    fix: backend urls

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -6,8 +6,8 @@ export const networks = {
 			erc1155: "0x1A511793FE92A62AF8bC41d65d8b94d4c2BD22c3",
 			utility: "0x08925246669D150d5D4597D756A3C788eae2834B",
 		},
-		// backend: "https://testnet-api.sqwid.app",
-		backend: "http://localhost:8080",
+		backend: "https://testnet-api.sqwid.app",
+		// backend: "http://localhost:8080",
 	},
 	reef_mainnet: {
 		rpc: "wss://rpc.reefscan.com/ws",
@@ -16,8 +16,8 @@ export const networks = {
 			erc1155: "0x0601202b75C96A61CDb9A99D4e2285E43c6e60e4",
 			utility: "0xffb12A5f69AFBD58Dc49b4AE9044D8F20D131733",
 		},
-		// backend: "https://mainnet-api.sqwid.app",
-		backend: "http://localhost:8080",
+		backend: "https://mainnet-api.sqwid.app",
+		// backend: "http://localhost:8080",
 	},
 };
 
