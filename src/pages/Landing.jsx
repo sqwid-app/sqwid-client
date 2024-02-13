8890c8dcd85fe05bc0f47c428313d005c4cdad78
Anukul Pandey <anukul.030601@gmail.com>
2024-02-13 11:30:35 +0530

    feat: precede reefscan api call

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,17 @@
 import Wrapper from "@components/Default/Wrapper";
-import React from "react";
+import React, { useEffect } from "react";
 import HeroSection from "@components/LandingRedesign/HeroSection";
 import useCheckJWT from "@utils/useCheckJWT";
+import { convertREEFtoUSD } from "@utils/convertREEFtoUSD";
 
 const Landing = () => {
 	useCheckJWT();
+	useEffect(()=>{
+		const fetcher = async()=>{
+			console.log(await convertREEFtoUSD(10))
+		}
+		fetcher()
+	},[])
 	return (
 		<Wrapper landing>
 			<HeroSection />
--- src/utils/convertREEFtoUSD.js
+++ src/utils/convertREEFtoUSD.js
@@ -1,13 +1,20 @@
+import { REEF_PRICE_COINGECKO, REEF_PRICE_REEFSCAN } from "@constants/price";
 import axios from "axios";
 
 export const convertREEFtoUSD = async price => {
 	try {
-		let res = await axios(
-			"https://api.coingecko.com/api/v3/simple/price?ids=reef&vs_currencies=usd"
-		);
-		let conversionRate = res.data["reef"].usd;
-		return Number(price) * Number(conversionRate);
+		let res = await axios(REEF_PRICE_REEFSCAN);
+		let conversionRate = res.data["usd"] ?? 0;
+		if (conversionRate !== 0) {
+			return Number(price) * Number(conversionRate);
+		}
 	} catch (err) {
-		return null;
+		try {
+			let res = await axios(REEF_PRICE_COINGECKO);
+			let conversionRate = res.data["reef"].usd ?? 0;
+			return Number(price) * Number(conversionRate);
+		} catch (error) {
+			return null;
+		}
 	}
 };
