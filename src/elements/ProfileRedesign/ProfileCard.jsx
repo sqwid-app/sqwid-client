2e2716aa57bb0610d4fe383c55aaba9548f45e42
Anukul Pandey <anukul.030601@gmail.com>
2024-02-15 17:04:12 +0530

    feat: loader in profile section

--- src/elements/ProfileRedesign/ProfileCard.jsx
+++ src/elements/ProfileRedesign/ProfileCard.jsx
@@ -43,6 +43,7 @@ import ChevronRight from "@static/svg/ChevronRight";
 import { NavLink } from "react-router-dom";
 import Background from "./Background";
 import { useErrorModalHelper } from "@elements/Default/ErrorModal";
+import LoadingIcon from "@static/svg/LoadingIcon";
 // import CardSectionContainer from "@elements/Default/CardSectionContainer";
 // import { fetchUserItems } from "@utils/marketplace";
 // import OnSaleSection from "./Sections/OnSaleSection";
@@ -778,6 +779,8 @@ const ProfileCard = () => {
 							) : (
 								<OnSaleSection/>
 							)} */}
+							{quickItems.length?
+							
 							<QuickSection
 								items={quickItems}
 								title={
@@ -790,7 +793,14 @@ const ProfileCard = () => {
 										? `?tab=Available`
 										: `?tab=On%20Sale`
 								}
-							/>
+							/>:
+							<div style={{position:"absolute",marginTop:"500px",marginLeft:"200px"}}>
+						<LoadingContainer>
+							<LoadingIcon size={128} />
+						</LoadingContainer>
+							</div>
+							
+						}
 						</ProfileWrapper>
 					) : (
 						<Header>Loading...</Header>
