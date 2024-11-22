439bda3eed46ed3be8542aa882b0207485b4faa9
Anukul Pandey <anukul.030601@gmail.com>
2024-11-22 12:00:42 +0530

    chore: undo dev env

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -6,8 +6,8 @@ export const networks = {
 			erc1155: "0x9FdEb478A27E216f80DaEE0967dc426338eD02f2",
 			utility: "0x8E7Ef6bD28cD9bDb6DBf105140958ac03EeC371A",
 		},
-		// backend: "https://sqwid-api-testnet.reefscan.info",
-		backend: "http://localhost:80",
+		backend: "https://sqwid-api-testnet.reefscan.info",
+		// backend: "http://localhost:80",
 	},
 	reef_mainnet: {
 		rpc: "wss://rpc.reefscan.com/ws",
@@ -16,8 +16,8 @@ export const networks = {
 			erc1155: "0x0601202b75C96A61CDb9A99D4e2285E43c6e60e4",
 			utility: "0xffb12A5f69AFBD58Dc49b4AE9044D8F20D131733",
 		},
-		// backend: "https://sqwid-api-mainnet.reefscan.info",
-		backend: "http://localhost:80",
+		backend: "https://sqwid-api-mainnet.reefscan.info",
+		// backend: "http://localhost:80",
 	},
 };
 
