47529346212b1b0e226ce296481c5b69fa033064
Anukul Pandey <anukul.030601@gmail.com>
2025-07-31 01:18:14 +0530

    fix: shortenIfAddress

--- src/utils/shortenIfAddress.js
+++ src/utils/shortenIfAddress.js
@@ -1,6 +1,6 @@
 import { truncateAddress } from "./textUtils";
 
-const shortenIfAddress = name =>
-	name.length === 42 && name.startsWith("0x") ? truncateAddress(name) : name;
+const shortenIfAddress = (name) => name && name.length === 42 && name.startsWith("0x") ? truncateAddress(name) : name;
+
 
 export default shortenIfAddress;
